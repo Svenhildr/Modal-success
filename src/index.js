058a2ff6a1a82896abf1769ce8573753acd5fa9a
Svenhildr <julie.poissonnier.1989@gmail.com>
2024-10-04 15:21:26 +0200

    update README

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom"; // Assure-toi d'importer ReactDOM
-import Modal from "./lib/Modalsuccess";
+import Modal from "./Modalsuccess";
 
-const TestModal = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-
-    const openModal = () => {
-        setIsModalVisible(true);
-    };
-
-    const closeModal = () => {
-        setIsModalVisible(false);
-    };
-
-    return (
-        <div>
-            <button onClick={openModal}>Show Modal</button>
-            <Modal isVisible={isModalVisible} onClose={closeModal} message="Employee Created!" />
-        </div>
-    );
-};
-
-ReactDOM.render(<TestModal />, document.getElementById("root"));
+export { Modal };
